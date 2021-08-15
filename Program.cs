f3775ff8204835ce945e44c057ce2484f4b6d1e2
Keith Adams <kma@Pamelas-MacBook-Air.local>
2021-08-15 15:06:36 -0700

    Some more node types.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
 var b = new Block(new List<Statement>());
+b.addStatement(new IfStatement(new VarExpr("b"),
+    new BuiltinCallStatement("foo", new List<Expression>())));
 Console.WriteLine("Statement:" + b.emit());
--- YulNode.cs
+++ YulNode.cs
@@ -11,13 +11,71 @@ class Block : Statement {
         statements_ = statements;
     }
     override public string emit() {
-        var s = "";
+        var s = "{";
         foreach (var stmt in statements_) {
             s += stmt.emit();
         }
-        return s;
+        return s + "}";
     }
     public void addStatement(Statement s){
-        statements_.Append(s);
+        statements_.Add(s);
+    }
+}
+
+abstract class Expression : Node {
+
+}
+
+class BuiltinCallExpr : Expression {
+    string name_;
+    List<Expression> params_;
+
+    public BuiltinCallExpr(string name, List<Expression> p){
+        name_ = name;
+        params_ = p;
+    }
+
+    public override string emit() {
+        var s = name_ + "(";
+        for (int i = 0; i < params_.Count; i++)
+        {
+            var p = params_[i];
+            s += p.emit();
+            if (i < params_.Count - 1) s += ", ";
+        }
+        return s + ")";
+    }
+}
+
+class BuiltinCallStatement : Statement {
+    private BuiltinCallExpr expr_;
+    public BuiltinCallStatement(string name, List<Expression> pa){
+        expr_ = new BuiltinCallExpr(name, pa);
+    }
+
+    public override string emit() {
+        return expr_.emit();
+    }
+}
+class VarExpr : Expression {
+    private string name_;
+    public VarExpr(string name){
+        name_ = name;
+    }
+    override public string emit() {
+        return name_;
+    }
+}
+
+class IfStatement : Statement {
+    private Expression cond_;
+    private Statement body_;
+    public IfStatement(Expression cond, Statement body){
+        cond_ = cond;
+        body_ = body;
+    }
+
+    override public string emit() {
+        return "if (" + cond_.emit() + ") " + body_.emit();
     }
 }
